fa0188ebf199b5e377fa90fdd75bcf149856cbec
Josh Greiff <josh@greiff.org>
2022-10-02 21:10:24 -0400

    MVC

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from . models import Post
+# Create your views here.
+
+#CRUD views
+def post_list_view(request):
+
+    post_objects = Post.objects.all()
+
+    context = {
+        'post_objects': post_objects
+    }
+
+    return render(request, "posts/index.html", context)
